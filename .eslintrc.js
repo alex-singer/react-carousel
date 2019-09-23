3dd87917ed640b43419d113c6d172a2ab12d3a08
Alex Singer <alex@asinger.me>
2019-09-23 07:42:36 -0500

    Add Jest setup file

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    plugins: ["react"],
+    extends: ["eslint:recommended", "plugin:react/recommended"],
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: "module",
+        ecmaFeatures: {
+            "jsx": true,
+        },
+    },
+    env: {
+        node: true,
+    },
+    rules: {
+        "comma-dangle": ["error", "always-multiline"],
+    },
+};
